913bb43218c4ff45357f1fff311998b14bbdbdf5
Tu Nombre <tu_correo@ejemplo.com>
2025-10-29 21:24:16 -0500

    Force @ alias via jsconfig + webpack alias

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  experimental: { serverActions: true }
+  experimental: { serverActions: true },
+  webpack: (config) => {
+    // Alias '@' => raíz del proyecto
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
 };
+
 module.exports = nextConfig;
